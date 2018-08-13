fdaa86254fcc165cd4b8d7d921f6e4ec970848d5
Kevin-Kip <masterfork5@gmail.com>
2018-08-14 00:46:57 +0300

    Added code to generate key

---
+++ main.py
@@ -0,0 +1,27 @@
+import random
+
+
+class Key:
+    def __init__(self, key=''):
+        if key == '':
+            self.key = self.generate()
+        else:
+            self.key = key.lower()
+
+    def generate(self):
+        key = ''
+        portion = ''
+        check_digit_count = 0
+        characters = 'abcdefghijklmnopqrstuvwxyz0123456789'
+
+        while True:
+            while len(key) < 25:
+                character = random.choice(characters)
+                key += character
+                portion += character
+                if len(portion) == 4:
+                    key += '-'
+                    portion = ''
+
+            return key
+
